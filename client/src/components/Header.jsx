b8575086f207f28187a1efcc354264288ce386c6
Cristian <cristiankhan23@gmail.com>
2023-12-18 15:38:20 -0300

    Add styles for username log

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -57,15 +57,19 @@ const Header = ({ onViewChange, isLoggedIn, authenticatedUser }) => {
           <h2>The Hunger Games</h2>
         </section>
       </div>
+      <div className='wellcome-msg'>
+        {isLoggedIn && (<p>Hello hunger {authenticatedUser}</p>)}  
+      </div>
       <div className='container-login-button'>
         <button className="login-button" onClick={() => handleLoginClick()}>
           Login
           <div className="icon">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-              <path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path>
+            </svg>
           </div>
         </button>
-        {isLoggedIn && (<p>Hello hunger {authenticatedUser}!</p>)}
       </div>
     </header>
   );
