939673b596398aa4eb03c3bb1fa19e8c235573bc
Cristianunrc <cristiankhan23@gmail.com>
2024-10-14 21:49:36 -0300

    Create new_district to instances district

--- tests/test_district.py
+++ tests/test_district.py
@@ -3,9 +3,11 @@ import pytest
 from game.logic.district import District
 from game.logic.tribute import Tribute, LIFE_DEFAULT, COWARDICE_DEFAULT
 
+def new_district():
+    return District()
 
 def test_set_config_tributes():
-    district = District()
+    district = new_district()
     district.set_config(50, 15, 6, 0, 5, 3)
     list_tributes = district.tributes
     for i in range(len(list_tributes)):
@@ -15,9 +17,8 @@ def test_set_config_tributes():
         assert list_tributes[i].district == 0
         assert list_tributes[i].cowardice == 3
 
-
 def test_set_config_random_tributes():
-    district = District()
+    district = new_district()
     num_district = 4
     district.set_config_random(num_district)
     for i in range(district.cant_tributes):
@@ -26,24 +27,21 @@ def test_set_config_random_tributes():
         assert district.tributes[i].district == num_district
         assert district.tributes[i].cowardice == COWARDICE_DEFAULT
 
-
 def test_add_tribute_valid():
-    district = District()
+    district = new_district()
     district.cant_tributes = 0
     tribute = Tribute()
     district.add_tribute(tribute)
     assert tribute in district.tributes
 
-
 def test_add_tribute_invalid():
-    district = District()
+    district = new_district()
     tribute = "Isn't a tribute"
     with pytest.raises(ValueError):
         district.add_tribute(tribute)
 
-
 def test_remove_tribute():
-    district = District()
+    district = new_district()
     t1 = Tribute()
     district.cant_tributes = 0
     district.add_tribute(t1)
@@ -51,17 +49,15 @@ def test_remove_tribute():
     district.remove_tribute(t1)
     assert len(district.tributes) == 0
 
-
 def test_get_number_district():
-    district = District()
+    district = new_district()
     district.number_district = 1
     expect = district.number_district
     res = district.get_number_district()
     assert expect == res
 
-
 def test_get_cant_tribute():
-    district = District()
+    district = new_district()
     district.cant_tributes = 4
     expect = district.cant_tributes
     res = district.get_cant_tribute()
