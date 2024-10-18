5083c2aa0b80f30c86fb424875edc863d3917771
Cristianunrc <cristiankhan23@gmail.com>
2024-10-18 14:33:50 -0300

    Refactor endpoints, delete comments and blank spaces

--- app/api/user_resource.py
+++ app/api/user_resource.py
@@ -14,57 +14,46 @@ class Structure(Resource):
         inspector = inspect(db.engine)
         tables = inspector.get_table_names()
         table_info = {}
-
         for table in tables:
             columns = inspector.get_columns(table)
             column_names = [column['name'] for column in columns]
             table_info[table] = column_names
-
         return jsonify(table_info)
         
 class Register(Resource):
     def post(self):
         global is_user_authenticated
-
         data = request.get_json()
         name = data.get('name')
         password = data.get('password')
         confirm_password = data.get('confirmPassword')
-
-        if password != confirm_password:
-            return {'error' : 'Las contraseñas no coinciden.'}, 401
-        
-        if is_user_authenticated:
-            return {'error': 'No puedes registrar una cuenta si estás logueado.'}, 400
-
         if name is None or password is None or name == '' or password == '':
-            return {'message': 'Nombre de usuario o contraseña obligatorios.'}, 400
-        else:
-            user = User()
-            try:
-                user.add_user(name, password)
-                return {'message': 'Usuario registrado con éxito.'}, 200
-            except IntegrityError as integrity_error:
-                db.session.rollback() #se revierte la transaccion
-                return {'error': 'El nombre de usuario ya esta en uso.'}, 400
+            return { 'message': 'Nombre de usuario o contraseña obligatorios.' }, 400
+        if password != confirm_password:
+            return { 'error' : 'Las contraseñas no coinciden.' }, 401
+        if is_user_authenticated:
+            return { 'error': 'No puedes registrar una cuenta si estás logueado.' }, 400
+        user = User()
+        try:
+            user.add_user(name, password)
+            return { 'message': 'Usuario registrado con éxito.' }, 200
+        except IntegrityError as integrity_error:
+            db.session.rollback()
+            return { 'error': 'Nombre de usuario en uso, prueba con otro.' }, 400
 
 class Login(Resource):
     def post(self):
         global is_user_authenticated
-
-        data = request.get_json()  
+        data = request.get_json()
         user = User.query.filter_by(username=data['name'], password=data['password']).first()
-        
         if user:
             if is_user_authenticated:
-                return {'error': 'No puedes iniciar una sesión mientras estés logueado.'}, 400
-            
+                return { 'error': 'No puedes iniciar sesión mientras estés logueado.' }, 400         
             is_user_authenticated = True
             access_token = create_access_token(identity=user.id, expires_delta=timedelta(hours=5)) #se crea un token unico de acceso
-            return {'message': 'Inicio de sesión exitoso.',
-                    'access_token': access_token}, 200
-        else:
-            return {'error': 'Nombre de usuario o constraseña incorrectos.'}, 401
+            return { 'message': 'Inicio de sesión exitoso.',
+                     'access_token': access_token }, 200
+        return { 'error': 'Nombre de usuario o constraseña incorrectos.' }, 401
 
     @jwt_required()
     def get(self):
@@ -72,23 +61,21 @@ class Login(Resource):
             user_id = get_jwt_identity()
             user = User.query.get(user_id)
             if user:
-                response = {'message': 'Token de acceso válido', 
-                            'user_id': user_id, 
-                            'username': user.username}, 200
-                return  response
+                return { 'message': 'Token de acceso valido', 
+                         'user_id': user_id, 
+                         'username': user.username }, 200
         except Exception as event:
-            return {'error': 'Token de acceso no valido'}, 401
+            return { 'error': 'Token de acceso no valido' }, 401
         
 class UserGet(Resource):
     def post(self):
         data = request.get_json()
         user = User.query.filter_by(username=data['name']).first()
-        
         if user:
             return user.json(), 200
         else:
-            return {'error': 'Usuario no encontrado.'}, 404
-        
+            return { 'error': 'Usuario no encontrado.' }, 404
+
 class SelectPj(Resource):
     def post(self):
         data = request.get_json()
@@ -98,27 +85,20 @@ class SelectPj(Resource):
 class UserIdGet(Resource):
     def post(self):
         data = request.get_json()
-        user = User.query.filter_by(username=data['name']).first()
-        
+        user = User.query.filter_by(username=data['name']).first()        
         if user:
-            result = user.get_id()
-            response = {'user_id': result}
-            return response, 200
-        else:
-            return {'error': 'Usuario no encontrado.'}, 404
+            return { 'user_id': user.get_id() }, 200
+        return { 'error': 'Usuario no encontrado.' }, 404
 
 class Logout(Resource):
-    @jwt_required() #asegura que solo users autenticados puedan cerrar sesion
+    @jwt_required()
     def post(self):
         global is_user_authenticated
-
-        user_id = get_jwt_identity() #recupera el ID del usuario a traves del token
+        user_id = get_jwt_identity()
         user = User.query.get(user_id)
-
-        if user: 
-            response = make_response() #crea una respuesta vacia
-            unset_jwt_cookies(response) #elimina cookies del token de acceso
+        if user:
+            response = make_response()
+            unset_jwt_cookies(response)
             is_user_authenticated = False
-            return {'message': 'Cierre de sesión exitoso.'}, 200
-        else:
-            return {'error': 'No hay un usuario logueado.'}, 404
+            return { 'message': 'Cierre de sesión exitoso.' }, 200
+        return { 'error': 'No hay un usuario logueado.' }, 404
