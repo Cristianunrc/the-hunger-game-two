4edd1cc87d6d3f09c6b31827ce776bf9e0f80f8c
Cristianunrc <cristiankhan23@gmail.com>
2024-10-18 19:58:03 -0300

    Delete blank spaces and unnecesary else condition

--- game/logic/board.py
+++ game/logic/board.py
@@ -19,7 +19,6 @@ DIRECTIONS = [
 
 
 class Board:
-
     # Initializes an instance of the Board class with dimensions X and Y
     def __init__(self, rows, columns):
         self.rows = rows
@@ -36,7 +35,6 @@ class Board:
     # Converts a string representing a board into an instance of the Board class.
     def from_string(board_str):
         from game.logic.district import District
-
         rows = board_str.split("\n")
         n_rows = len(rows)
         if n_rows < 1:
@@ -45,15 +43,13 @@ class Board:
         n_cols = len(matrix[0])
         if n_cols < 1:
             raise ValueError(f"Invalid number of columns: {n_cols}")
-
         new_board = Board(n_rows, n_cols)
         districts, neutrals, order = [], [], [0, 1, 2, 3, 4, 5]
         for i in range(6):
             district = District()
             district.number_district = i
             districts.append(district)
-
-        char_to_class = {  # Dicctionary that mapping chars to class
+        char_to_class = { #Dicctionary that mapping chars to class
             "w": Weapon, "sp": Spear, "sw": Sword, "wo": Bow,
             "po": PotionPoison, "pl": PotionLife, "pf": PotionForce
         }
@@ -68,7 +64,6 @@ class Board:
                     neutrals.append(tribute)
                 elif char.startswith(("t", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",)
                                      ):
-                    # Check if it's a tribute character ('t') followed by a number
                     try:
                         tribute_number = int(char[1])
                         if 0 <= tribute_number <= 5:
@@ -86,21 +81,19 @@ class Board:
                 elif char == "  ":
                     pass
                 elif char in char_to_class:
-                    item_class = char_to_class[char]  # recovered class of item
-                    item = item_class()  # create instance of class item
+                    item_class = char_to_class[char] #recovered class of item
+                    item = item_class() #create instance of class item
                     new_board.get_element(row, col).put_item(item)
                     item.pos = (row, col)
                 elif char != "":
                     raise ValueError(f"Invalid character in board string: {char}")
-
         return [new_board, districts, neutrals, order]
 
     # Gets the cell at a specific position on the board.
     def get_element(self, row, column):
         if 0 <= row < self.rows and 0 <= column < self.columns:
             return self.board[row][column]
-        else:
-            raise ValueError(f"Invalid row or column index: ({row}, {column})")
+        raise ValueError(f"Invalid row or column index: ({row}, {column})")
 
     # Places a tribute at a specific position on the board.
     def put_tribute(self, row, column, tribute):
@@ -179,7 +172,7 @@ class Board:
             if cell.get_state() == State.FREE:
                 res += "  "
             else:
-                res += cell.__str__()  # Use the __str__ method of the cell
+                res += cell.__str__()
             if col < columns - 1:
                 res += "|"
         return res
@@ -201,14 +194,10 @@ class Board:
     # Gets the cells adjacent to a specific position on the board.
     def get_adjacent_positions(self, row, column):
         adjacent_positions = []
-
-        # Define the directions for adjacent cells, including the corners.
         for dr, dc in DIRECTIONS:
             new_row, new_column = row + dr, column + dc
-
             if 0 <= new_row < self.rows and 0 <= new_column < self.columns:
                 adjacent_positions.append((new_row, new_column))
-
         return adjacent_positions
 
     # Check if a position on the board is valid and free.
@@ -221,22 +210,18 @@ class Board:
     def get_adjacents_cells(self, x, y):
         if not (0 <= x < self.rows and 0 <= y < self.columns):
             raise ValueError(f"Coordinates ({x}, {y}) are out of bounds")
-
         list_pos = self.get_adjacent_positions(x, y)
         adjacent_cells = []
-
         for pos in list_pos:
             x, y = pos
             if 0 <= x < self.rows and 0 <= y < self.columns:
                 adjacent_cells.append(self.get_element(x, y))
-
         return adjacent_cells
 
     # Get the free adjacent cells to a specific position on the board.
     def get_free_adjacents_cells(self, x, y):
         if not (0 <= x < self.rows and 0 <= y < self.columns):
             raise ValueError(f"Invalid position: x={x}, y={y} is out of range.")
-
         adjacents_cells = self.get_adjacents_cells(x, y)
         free_adjacents = []
         for cell in adjacents_cells:
@@ -248,16 +233,13 @@ class Board:
     def get_free_adjacents_positions(self, x, y):
         if not (0 <= x < self.rows and 0 <= y < self.columns):
             raise ValueError(f"Invalid position: x={x}, y={y} is out of range.")
-
         free_positions = []
         for dr, dc in DIRECTIONS:
             new_row, new_column = x + dr, y + dc
-
             if 0 <= new_row < self.rows and 0 <= new_column < self.columns:
                 element = self.get_element(new_row, new_column)
                 if element.get_state() != State.TRIBUTE:
                     free_positions.append((new_row, new_column))
-
         return free_positions
 
     # Makes a random choice of a free adjacent position for a tribute without
@@ -265,19 +247,15 @@ class Board:
     def random_choice(self, tribute):
         x, y = tribute.pos
         free_adjacents_pos = self.get_free_adjacents_positions(x, y)
-
         if tribute.past_pos in free_adjacents_pos:
             free_adjacents_pos.remove(tribute.past_pos)
-
         if not free_adjacents_pos:
             raise ValueError(
                 f"No available free adjacent positions for Tribute {tribute}"
             )
-
         pos = random.choice(free_adjacents_pos)
         while self.board[pos[0]][pos[1]].get_state() == State.ITEM:
             pos = random.choice(free_adjacents_pos)
-
         return pos
 
 
